4434de1599a83f516a76864c6dde2283f5214c01
Andrew Harris <andrew.harris@live.com>
2024-04-06 23:53:04 -0400

    Add files via upload

---
+++ Graphing.py
@@ -0,0 +1,30 @@
+'''
+Andrew Harris
+CIS123
+5.4 Guided Practice
+'''
+
+from matplotlib import pyplot as plt
+import csv
+
+x=[]
+y=[]
+
+with open("c:\\Users\\andhar5173\\Desktop\\Excel Sheet.csv")as myfile:
+
+    reader=csv.reader(myfile)
+    for row in reader:
+        try:
+            x.append(row[3])
+            y.append(float(row[11])*100)
+        except:
+            y.append(80)
+
+    plt.ylim(50,90)
+    plt.plot(x,y, label="Loaded from example.csv")
+    plt.xlabel("county")
+    plt.ylabel("vote %")
+    plt.title("Voting percentage by precinct in Fairfax county")
+    plt.legend()
+    plt.show()
+
